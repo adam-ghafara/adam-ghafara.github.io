1c429fd2dddeb0fc053675904abe972df0d59df8
adam-ghafara <adam.ghafara.job@gmail.com>
2023-10-02 11:07:36 +0700

    Pembaruan

--- openlyr/js/penjalan.js
+++ openlyr/js/penjalan.js
@@ -1,18 +1,13 @@
-import './style.css';
-import Map from 'ol/Map.js';
-import OSM from 'ol/source/OSM.js';
-import TileLayer from 'ol/layer/Tile.js';
-import View from 'ol/View.js';
+import {map} from './config/config.js';
+import {onClosePopupClick,onDeleteMarkerClick,onSubmitMarkerClick,onMapClick,onMapPointerMove,disposePopover} from './controller/popup.js';
+import {onClick} from 'https://jscroot.github.io/element/croot.js';
+import {getAllCoordinates} from './controller/flo.js';
 
-const map = new Map({
-  target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM(),
-    }),
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2,
-  }),
-});
+onClick('popup-closer',onClosePopupClick);
+onClick('insertmarkerbutton',onSubmitMarkerClick);
+onClick('hapusbutton',onDeleteMarkerClick);
+onClick('hitungcogbutton',getAllCoordinates);
+
+map.on('click', onMapClick);
+map.on('pointermove', onMapPointerMove);
+map.on('movestart', disposePopover);
