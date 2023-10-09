c9e252473719351f24d3572f04ac848da5d9e7f2
Adam Ghafara <93985164+adam-ghafara@users.noreply.github.com>
2023-10-09 16:57:47 +0700

    Update collect_api.js

--- collect_api.js
+++ collect_api.js
@@ -6,7 +6,9 @@ async function ambilDataDariAPI() {
     try {
         const response = await fetch('https://asia-southeast2-gis-tester-401506.cloudfunctions.net/function-1');
         const data = await response.text();
-        hasil.innerHTML = `<p>${data}</p>`;
+        hasil.innerHTML = `<p>${data.Name}</p>`;
+        hasil.innerHTML = `<p>${data.Coordinate}</p>`;
+        hasil.innerHTML = `<p>${data.Type}</p>`;
     } catch (error) {
         console.error('Terjadi kesalahan:', error);
         hasil.innerHTML = '<p>Terjadi kesalahan saat mengambil data dari API.</p>';
