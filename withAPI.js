d3fd5f94c85c6c5d2975200a6516da19b2da4645
adam-ghafara <adam.ghafara.job@gmail.com>
2023-10-16 08:59:19 +0700

    TAMBAH API

--- withAPI.js
+++ withAPI.js
@@ -1,5 +1,5 @@
 import { get } from "https://jscroot.github.io/api/croot.js";
 import {setInner,addChild } from "https://jscroot.github.io/element/croot.js";
-import {URLGeoJson,responseData} from "./js/collect_api.js";
+import {URLGeoJson,responseData} from "./collect_api.js";
 
 get(URLGeoJson,responseData);
